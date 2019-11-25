fb1800b7060e49e3b6c3efde5c8807ea28594f59
ayanendu de <>
2019-11-25 17:02:55 -0500

    change

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -32,8 +32,8 @@ service { 'mysql':
 # }
 
 # ensure info.php file exists
-file { '/var/www/html/info.php':
-  ensure => file,
-  content => '<?php  phpinfo(); ?>',    # phpinfo code
-  require => Package['apache2'],        # require 'apache2' package before creating
-}
+# file { '/var/www/html/info.php':
+#   ensure => file,
+#   content => '<?php  phpinfo(); ?>',    # phpinfo code
+#   require => Package['apache2'],        # require 'apache2' package before creating
+# }
