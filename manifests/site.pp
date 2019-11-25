235ccbc8c274e2513b5fa075e9467101edc9cf59
ayanendu de <>
2019-11-25 00:51:17 -0500

    site ch

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,5 +27,8 @@ File { backup => false }
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
-  #   class { 'my_class': }
+  $packages = ['vim', 'git', 'curl']  
+    package { $packages: 
+      ensure => "installed" 
+    }
 }
