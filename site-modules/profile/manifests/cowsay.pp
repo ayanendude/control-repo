1a460c22e471146baf9e10e19144f911ad5193ae
ayanendu de <>
2019-11-26 22:01:36 -0500

    p

--- site-modules/profile/manifests/cowsay.pp
+++ site-modules/profile/manifests/cowsay.pp
@@ -1,4 +1,14 @@
 class profile::cowsay {
+
+  package { 'rubygems':
+    ensure => present,
+  }
+
+  package { 'puppet-lint':
+    ensure   => '1.1.0',
+    provider => 'gem',
+    require  => Package['rubygems'],
+  }
   package { 'cowsay':
     ensure   => present,
     provider => 'gem',
